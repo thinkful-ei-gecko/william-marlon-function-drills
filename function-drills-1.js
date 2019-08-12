3bcb3ff6715b24782a2ca89233dd46c67721200f
Marlon Agno <mdj.agno@gmail.com>
2019-08-12 16:22:10 -0400

    made revisions with TA

--- function-drills-1.js
+++ function-drills-1.js
@@ -17,15 +17,14 @@ console.log(greetingOne);*/
   return `My name is ${name} and I'm ${age} years old.`;
 }*/
 
-function getYearOfBirth(age){
+/*function getYearOfBirth(age){
   return 2019 - age;
 }
 
-const yearOfBirth = getYearOfBirth(25);
-
-function createGreeting(name) {
-  return `My name is ${name} and I was born in ${yearOfBirth}`;
-}
+function createGreeting(name, age){
+const yob = getYearOfBirth(age); 
+return `My name is ${name} and I was born in ${yob}`;
+}*/
 
 function getYearOfBirth(age) {
   if (age < 0) {
@@ -34,9 +33,27 @@ function getYearOfBirth(age) {
   return 2019 - age;
 }
 
-try {
+/*try {
   const greetingOne = createGreeting('Marlon', -25);
 }
 catch(err) {
   console.log('Age cannot be negative');
+}*/
+
+//getYearOfBirth(-25);
+
+function createGreeting(name, age) {
+  if(name === undefined || age === undefined){
+    throw new Error('Argument not valid');
+  }
+  if (age < 0){
+    throw new Error('Age cannot be negative');
+  }
+  if (typeof age !== 'number'){
+    throw new Error('Age must be a number');
+  }
+  const yob = getYearOfBirth(age); 
+  return `My name is ${name} and I was born in ${yob}`;
 }
+
+console.log(createGreeting('Marlon', -25));
