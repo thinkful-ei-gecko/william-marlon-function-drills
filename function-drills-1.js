0cc8b2002abc927f1b56d38436ffb596f87a7151
William Bae <wbae912@gmail.com>
2019-08-12 12:29:16 -0700

    try catch added

--- function-drills-1.js
+++ function-drills-1.js
@@ -17,13 +17,26 @@ console.log(greetingOne);*/
   return `My name is ${name} and I'm ${age} years old.`;
 }*/
 
-function createGreeting(name, age) {
-  const yearOfBirth = 2019 - age;
-  return `My name is ${name} and I was born in ${yearOfBirth}`;
-}
-
 function getYearOfBirth(age){
   return 2019 - age;
 }
 
+const yearOfBirth = getYearOfBirth(25);
 
+function createGreeting(name) {
+  return `My name is ${name} and I was born in ${yearOfBirth}`;
+}
+
+function getYearOfBirth(age) {
+  if (age < 0) {
+    throw new Error('Age cannot be negative');
+  }
+  return 2019 - age;
+}
+
+try {
+  const greetingOne = createGreeting('Marlon', -25);
+}
+catch(err) {
+  console.log('Age cannot be negative');
+}
