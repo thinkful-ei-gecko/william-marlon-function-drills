4156c2cec9467359d4d42d16e307fdf3940c8f36
Marlon Agno <mdj.agno@gmail.com>
2019-08-12 14:49:55 -0400

    added createGreeting function

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,5 +1,7 @@
 'use strict';
 
-function hiName(name, age) {
-    console.log(`Hi my name is ${name} and I'm ${age} years old.`)
+function createGreeting(){
+    console.log('My name is Marlon and I\'m 25 years old.');
 }
+
+createGreeting();
