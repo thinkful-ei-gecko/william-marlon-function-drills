575df8d73b3d36dd594174a39b69bf37f33fb275
William Bae <wbae912@gmail.com>
2019-08-12 14:25:55 -0700

    days in month function

--- william-marlon-function-drills-2.js
+++ william-marlon-function-drills-2.js
@@ -48,6 +48,34 @@ console.log(beyond(0));*/
   }    
   return codeResult.join('');
 }
-
 console.log(decode('craft block argon meter bells brown croon droop'));*/
 
+/*function howManyDays (month, leapYear) {
+  switch(month) {
+  case 'January':
+  case 'March':
+  case 'May':
+  case 'July':
+  case 'August':
+  case 'October':
+  case 'December':
+    return `${month} has 31 days`;
+    break;
+  case 'April': 
+  case 'June':
+  case 'September':
+  case 'November':
+    return `${month} has 30 days`;
+    break;
+  case 'February':
+    if (leapYear === true) {
+      return `${month} has 29 days`;
+    } else {
+      return `${month} has 28 days`;
+      break;
+    }
+  default:
+    throw new Error('Must provide a valid month.');
+  }
+}
+console.log(howManyDays('Marlon'));*/
