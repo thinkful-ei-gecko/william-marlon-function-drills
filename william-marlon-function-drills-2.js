cd4d5ca5c31095900bd253a2f45cb964e2240b4f
William Bae <wbae912@gmail.com>
2019-08-12 13:37:59 -0700

    beyond function added

--- william-marlon-function-drills-2.js
+++ william-marlon-function-drills-2.js
@@ -1,6 +1,28 @@
 'use strict';
 
-function jediName(firstName, lastName){
+/*function jediName(firstName, lastName) {
   return lastName.slice(0,3) + firstName.slice(0,2);
 }
+
+let myName = jediName('William', 'Bae');
+console.log(myName);*/
+
+/*function beyond(num) {
+  if (num === Infinity || num === -Infinity) {
+    console.log('And beyond');
+  }
+  if (isFinite(num) && num > 0) {
+    console.log('To infinity');
+  }
+  if (isFinite(num) && num < 0) {
+    console.log('To negative infinity');
+  }
+  if (num === 0) {
+    console.log('Staying home');
+  }
+}
+console.log(beyond(-Infinity));
+console.log(beyond(20));
+console.log(beyond(-20));
+console.log(beyond(0));*/
 
